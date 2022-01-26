a211a052fe5a59edd10302583011d069ae7d5ea0
Bohdan-Dachkovsky <90333649+Bohdan-Dachkovsky@users.noreply.github.com>
2022-01-26 11:25:00 +0200

    I'm connect basiclightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,2 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const items = document.querySelector(`.gallery`);
+
+console.log(items);
+const galleryPicture = getGalleryPicture(galleryItems);
+
+items.insertAdjacentHTML("beforeend", galleryPicture);
+  console.log(getGalleryPicture(galleryItems));
+function getGalleryPicture(galleryItems) {
+  return  galleryItems.map(({priview, original})=> {
+      return  `<li><div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src="${original}"
+          data-source="large-image.jpg"
+          alt="Image description"
+        />
+      </a>
+    </div></li>`;
+  }).join(``);  
+
+};
+
+items.click(function(event) {
+  let el = event.target;
+  el.hasClass('basicLightbox') ? el.removeClass('basicLightbox') : el.addClass('basicLightbox');
+});
